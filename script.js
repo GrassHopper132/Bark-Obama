21fde47ac7121c196c8687d079e23d11e4048ee1
GrassHopper132 <crhymer101@gmail.com>
2026-08-02 14:11:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+age = 2
+pet = "dog"
+skills = ["barking", "running", "fetching", "playing", "begging", "finding socks"]
+strengths = [lots of energy, eats everything, loves everyone, energetic]
+
+input = input("Do you love dogs? ").strip().lower()
+
+
+const myButton = document.querySelector('.my-btn');
+
+
+myButton.addEventListener('click', function() {
+    alert('The button was clicked!');
+});
+
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button was clicked!');
+});
+
+const box = document.getElementById('myBox');
+box.addEventListener('mouseover', function() {
+    box.style.backgroundColor = 'blue';
+});
+document.getElementById('usernameInput').addEventListener('keyup', function(event) {
+    console.log('You typed: ' + event.target.value);
+});
