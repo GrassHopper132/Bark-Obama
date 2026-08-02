a511a91e060f7010959200ecf6592fa465a4d0db
GrassHopper132 <crhymer101@gmail.com>
2026-08-02 14:16:44 -0400

    Add barkObamaStats object to dogdata.js

---
+++ dogdata.js
@@ -0,0 +1,6 @@
+export const barkObamaStats = {
+    age: 2,
+    pet: "dog",
+    skills: ["barking", "running", "fetching", "playing", "begging", "finding socks"],
+    strengths: ["lots of energy", "eats everything", "loves everyone", "energetic"]
+};
